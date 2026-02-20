372d9592ed97d0da6156cf3bb83f81d57471fde5
Rafiyaahmed-bigdatalearner <rafiyaahmed010@gmail.com>
2026-02-20 14:26:59 +0400

    Update utils.py

--- lib/utils.py
+++ lib/utils.py
@@ -1,12 +1,20 @@
 from pyspark.sql import SparkSession
 from lib.ConfigReader import get_pyspark_config
 
-def get_spark_session(env):
-    if env == "LOCAL":
+def get_spark_session(env: str) -> SparkSession:
+    """
+    Returns a SparkSession based on the environment.
+
+    :param env: "LOCAL" or "PROD"
+    :return: SparkSession
+    """
+    if env.upper() == "LOCAL":
         return SparkSession.builder \
             .config(conf=get_pyspark_config(env)) \
-            .config('spark.driver.extraJavaOptions',
-                    '-Dlog4j.configuration=file:log4j.properties') \
+            .config(
+                'spark.driver.extraJavaOptions',
+                '-Dlog4j.configuration=file:log4j.properties'
+            ) \
             .master("local[2]") \
             .getOrCreate()
     else:
